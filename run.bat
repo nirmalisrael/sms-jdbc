a5835f13f663631b34602a93e89d7f690d8ce1df
nirmalisrael <nirmal.israel126@gmail.com>
2024-06-28 10:04:19 +0530

    Adding run.bat file to main

---
+++ run.bat
@@ -0,0 +1,8 @@
+@echo off
+javac -d .\bin src\main\java\com\algoriant\sms\model\Student.java
+javac -d .\bin -cp .\conf;.\lib\* src\main\java\com\algoriant\sms\service\DBService.java
+javac -d .\bin -cp .\lib\*;.\conf;.\bin src\main\java\com\algoriant\sms\dao\StudentDAO.java
+javac -d .\bin -cp .\lib\*;.\conf;.\bin src\main\java\com\algoriant\sms\dao\impl\StudentDAOImpl.java
+javac -d .\bin -cp .\bin;.\conf src\main\java\com\algoriant\sms\UIClass.java
+java -cp .\bin;.\lib\*;.\conf com.algoriant.sms.UIClass
+
